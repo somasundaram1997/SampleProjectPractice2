c281fb01aa280c83575a93f12c8387b2a29df2b8
somasundaram1997 <somasundarams23@gmail.com>
2022-08-04 21:16:53 +0530

    sundar code

--- src/main/java/org/test/git/SampleGit.java
+++ src/main/java/org/test/git/SampleGit.java
@@ -19,17 +19,15 @@ public class SampleGit {
 		WebElement txtPassword = driver.findElement(By.id("pass"));
 		txtPassword.sendKeys("989898");
 		
-		WebElement btnLogin = driver.findElement(By.name("login"));
-		btnLogin.click();
+		WebElement txtUsername4 = driver.findElement(By.xpath("//input[@type='text']"));
+		txtUsername.sendKeys("somasundaram@gamil.com");
+		String text1 = txtUsername4.getAttribute("value");
+		System.out.println(text1);
 		
-		WebElement txtUsername1 = driver.findElement(By.id("email"));
-		txtUsername.sendKeys("sunadarm");
-		
-		WebElement txtPassword1 = driver.findElement(By.id("pass"));
-		txtPassword.sendKeys("989898");
-		
-		WebElement btnLogin1 = driver.findElement(By.name("login"));
-		btnLogin.click();
+		WebElement txtPassword4 = driver.findElement(By.xpath("//input[@type='password']"));
+		txtPassword.sendKeys("123456789!");
+		String text3 = txtPassword4.getAttribute("value");
+		System.out.println(text3);
 
 		
 	}
